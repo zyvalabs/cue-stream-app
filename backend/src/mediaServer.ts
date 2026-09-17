dc2e25af736f00435e5eae3d1deb79788101f959
Zyva Labs <zyvalabs@gmail.com>
2026-09-17 16:59:44 +0530

    add debug event logging

--- backend/src/mediaServer.ts
+++ backend/src/mediaServer.ts
@@ -8,11 +8,11 @@ const config = {
     ping: 30,
     ping_timeout: 60,
   },
-http: {
-  port: 8000,
-  mediaroot: './media',
-  allow_origin: '*',
-},
+  http: {
+    port: 8000,
+    mediaroot: './media',
+    allow_origin: '*',
+  },
   trans: {
     ffmpeg: '/usr/bin/ffmpeg',
     tasks: [
@@ -24,8 +24,22 @@ http: {
     ],
   },
 };
+
 export function startMediaServer() {
   const nms = new NodeMediaServer(config);
+
+  nms.on('prePublish', (id: string, StreamPath: string, args: object) => {
+    console.log('[NMS] prePublish', id, StreamPath, args);
+  });
+
+  nms.on('postPublish', (id: string, StreamPath: string, args: object) => {
+    console.log('[NMS] postPublish', id, StreamPath, args);
+  });
+
+  nms.on('donePublish', (id: string, StreamPath: string, args: object) => {
+    console.log('[NMS] donePublish', id, StreamPath, args);
+  });
+
   nms.run();
   console.log('RTMP ingest server running on port 1935 (HTTP on 8000)');
 }
